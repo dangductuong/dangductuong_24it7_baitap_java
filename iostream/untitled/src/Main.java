c1b279c81ea633a3590d5fbbd0eb3e154d4acc75
dangductuong <tuongdd.24itb@vku.udn.vn>
2025-04-14 20:01:52 +0700

    Add files via upload

---
+++ iostream/untitled/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("input.txt");
+             FileOutputStream fos = new FileOutputStream("output.txt")) {
+
+            int byteData;
+            while ((byteData = fis.read()) != -1) {
+                fos.write(byteData);
+            }
+            System.out.println("Sao chép file thành công!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
